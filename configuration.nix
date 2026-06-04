10e0933c25efb3627ba51aae8be073b042387272
Lucas Aponso <s3896348@student.rmit.edu.au>
2026-06-04 18:22:25 +1000

    mod: moved to XFCE desktop environment. low memory usage.

--- configuration.nix
+++ configuration.nix
@@ -1,22 +1,107 @@
 { config, pkgs, ... }:
-
 {
   imports = [
     ./hardware-configuration.nix
-    ./modules/boot.nix
-    ./modules/network.nix
-    ./modules/locale.nix
-    ./modules/desktop.nix
-    ./modules/audio.nix
-    ./modules/fonts.nix
-    ./modules/shell.nix
-    ./modules/neovim.nix
-    ./modules/packages.nix
-    ./modules/users.nix
-    ./modules/virtualisation.nix
-    ./modules/services.nix
-    ./modules/git.nix
-    ./modules/nix.nix
+  ];
+
+  # Boot
+  # Replace with:
+  boot.loader.grub = {
+    enable = true;
+    device = "/dev/sda";  # or /dev/vda for QEMU — check with lsblk
+  };
+
+  # Network
+  networking.hostName = "nixos";
+  networking.networkmanager.enable = true;
+  networking.firewall.allowedTCPPorts = [ 22 ];
+  networking.firewall.enable = true;
+  
+  # Locale
+  time.timeZone = "Australia/Melbourne";
+  i18n.defaultLocale = "en_AU.UTF-8";
+
+  # Desktop
+  services.xserver = {
+    enable = true;
+    desktopManager.xfce.enable = true;
+    displayManager.lightdm.enable = true;
+  };
+
+  # VM guest tools (QEMU/KVM)
+  services.spice-vdagentd.enable = true;
+  services.qemuGuest.enable = true;
+
+  # Audio
+  services.pipewire = {
+    enable = true;
+    alsa.enable = true;
+    pulse.enable = true;
+  };
+
+  services.openssh = {
+    enable = true;
+    settings = {
+      PasswordAuthentication = true;
+      PermitRootLogin = "no";
+    };
+  };
+
+  # User
+  users.users.lucas = {
+    isNormalUser = true;
+    shell = pkgs.fish;
+    initialPassword = "Yoplait4X$#";
+  };
+  
+  programs.wireshark = {
+    enable = true;
+    package = pkgs.wireshark;
+  };
+
+  virtualisation.docker.enable = true;
+
+  users.users.lucas.extraGroups = [
+    "wheel"
+    "networkmanager"
+    "docker"
+    "wireshark"
+  ];
+
+  systemd.tmpfiles.rules = [
+    "d /etc/nixos 0755 lucas users"
+  ];
+
+  # Shell
+  programs.fish.enable = true;
+
+  # Packages
+  environment.systemPackages = with pkgs; [
+    git
+    vscode
+    curl
+    wget
+    neovim
+    fzf
+    ripgrep
+    htop
+    nmap
+    firefox
+    tmux
+    gcc
+    cmake
+    python3
+    docker
+    nodejs
+    unzip
+    minicom
+    tcpdump
+    netcat
+    docker-compose
+    wireshark
+    postman
+    whois
+    traceroute
   ];
 
   nixpkgs.config.allowUnfree = true;
--- modules/services.nix
+++
@@ -1,6 +0,0 @@
-{ config, pkgs, ... }:
-
-{
-  services.gnome.gnome-keyring.enable = true;
-  programs.wireshark.enable = true;
-}
